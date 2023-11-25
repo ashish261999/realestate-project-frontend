6e1be246a2805e98a041b0abc255a68cd06fe090
ashish ranjan <ashishranjanmahi07@gmail.com>
2023-11-25 12:05:07 +0530

    header created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './component/header';
+import {BrowserRouter , Routes , Route} from "react-router-dom";
+import Home from "./pages/home";
+import Profile from './pages/profile';
+import Signup from './pages/signup';
+import Signin from './pages/signin';
+import About from "./pages/about";
 
 function App() {
   return (
-    <div className="App">
-     hello from react
+    <div>
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/sign-in' element={<Signin/>}/>
+        <Route path='/sign-up' element={<Signup />}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/profile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div>
+      profle
+    </div>
+  )
+}
+
+export default Profile
