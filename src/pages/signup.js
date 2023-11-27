5dff50e46861af94febe80d2d02b307cbe317d2f
ashish ranjan <ashishranjanmahi07@gmail.com>
2023-11-27 10:24:15 +0530

    sign-up created with fully functional

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,31 +1,94 @@
-import React from 'react'
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { Link , useNavigate } from "react-router-dom";
 
 const Signup = () => {
-  return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>
-        Sign Up</h1>
-      <form className='flex flex-col gap-4'> 
-        <input type='username' placeholder='username' 
-        className='border p-3 rounded-lg' id='username'/>
-         <input type='email' placeholder='email' 
-        className='border p-3 rounded-lg' id="email"/>
-         <input type='password' placeholder='password' 
-        className='border p-3 rounded-lg' id='password'/>
-        <button className='bg-slate-700 text-white p-3
-        rounded-lg uppercase hover:opacity-95 
-        disabled:opacity-80'>Sign Up</button>
-      </form>
-      <div className='flex  gap-2 mt-5'>
-        <p>Have an account? </p>
-          <Link to={"/sign-in"}>
-          <span  className='text-blue-700'>Sign in </span>
-          </Link>
+   const [formData , setFormData] = useState({});
+   const [error , setError] = useState(null);
+   const [loading, setLoading] = useState(false);
+   const navigate = useNavigate();
+
+  const handlechange= (e)=>{
+    
+    setFormData({
+      
+      ...formData ,
+      [e.target.id]: e.target.value
+  });
+};
+
+  const handlesubmit = async(e)=>{
+    try{
+    setLoading(true);
+        e.preventDefault();
+        const  res = await fetch("http://localhost:8080/api/sign-up" ,{
+          method: "POST",
+          headers:{
+            "Content-Type" : "application/json",
+          },
+          body:JSON.stringify(formData),
+
+        });
+        const data = await res.json();
+        if(data.success === false){
+          setError(data.message);
+          setLoading(false);
+          return;
+        }
+          setLoading(false);
+        setError(null);
+        navigate("/sign-in")
+      }catch (error){
+        setError(error.message);
+        setLoading(false);
+      }
        
+  };
+ 
+  console.log(formData);
+
+  
+ 
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form onSubmit={handlesubmit} className="flex flex-col gap-4">
+        <input
+          type="username"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+          id="username"
+          onChange={handlechange}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+          onChange={handlechange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+          onChange={handlechange}
+        />
+        <button disabled={loading}
+          className="bg-slate-700 text-white p-3
+        rounded-lg uppercase hover:opacity-95 
+        disabled:opacity-80"
+        >
+        {loading ? "Loading...": "Sign Up"}
+        </button>
+      </form>
+      <div className="flex  gap-2 mt-5">
+        <p>Have an account? </p>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-700">Sign in </span>
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
